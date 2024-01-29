e5a0e8627d6d3f59cd579eee0c9c105ea5cebaf4
Timothy Chua <timothy.chua@22media.co>
2024-01-29 18:02:40 +0800

    Add custom smooth navigate scroll

--- src/lib/utils/navigate.ts
+++ src/lib/utils/navigate.ts
@@ -1,5 +1,40 @@
-const DOWN_Y_OFFSET = 300
+const DOWN_Y_OFFSET = 500
 const UP_Y_OFFSET = -900
+
+function customSmoothScroll(targetY: number, duration = 800) {
+  const startingY = window.scrollY
+  const startTime =
+    'now' in window.performance ? performance.now() : new Date().getTime()
+
+  function easeInOutQuad(
+    time: number,
+    start: number,
+    change: number,
+    duration: number
+  ) {
+    time /= duration / 2
+    if (time < 1) return (change / 2) * time * time + start
+    time--
+    return (-change / 2) * (time * (time - 2) - 1) + start
+  }
+
+  function scroll() {
+    const currentTime =
+      'now' in window.performance ? performance.now() : new Date().getTime()
+    const timeElapsed = currentTime - startTime
+
+    window.scrollTo(
+      0,
+      easeInOutQuad(timeElapsed, startingY, targetY - startingY, duration)
+    )
+
+    if (timeElapsed < duration) {
+      requestAnimationFrame(scroll)
+    }
+  }
+
+  scroll()
+}
 
 // https://stackoverflow.com/questions/24665602/scrollintoview-scrolls-just-too-far
 function navigateToElementId(id: string) {
@@ -15,7 +50,7 @@ function navigateToElementId(id: string) {
     y += UP_Y_OFFSET
   }
 
-  window.scrollTo({ top: y, behavior: 'smooth' })
+  customSmoothScroll(y)
 }
 
 export default navigateToElementId
