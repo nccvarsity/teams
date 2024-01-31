116022a36872540ba927aa68ace374ec25de81d8
Timothy Chua <timothy.chua@22media.co>
2024-01-31 22:18:41 +0800

    Clean up spelling & lint

--- src/app/sections/highlight/index.tsx
+++ src/app/sections/highlight/index.tsx
@@ -1,57 +1,52 @@
-"use client";
+'use client'
 
-import * as Scrollytelling from "@bsmnt/scrollytelling";
+import * as Scrollytelling from '@bsmnt/scrollytelling'
+import { useMemo } from 'react'
 
-import s from "./highlight.module.scss";
-import { useMemo } from "react";
+import s from './highlight.module.scss'
 
-const splitText = (text: string, wordClass?: string) => {
-  const wordsArray = text.split(" ");
+const splitTextFormat = (text: string, wordClass?: string) => {
+  const wordsArray = text.split(' ')
 
   const htmlWords = wordsArray.map((word, i) => {
-    const hasLineBreak = word.includes("\n");
+    const hasLineBreak = word.includes('\n')
 
     return (
       <span className={wordClass} key={i}>
         {word}
-        {i < wordsArray.length - 1 && " "}
+        {i < wordsArray.length - 1 && ' '}
         {hasLineBreak && <br />}
       </span>
-    );
-  });
+    )
+  })
 
-  return htmlWords;
-};
+  return htmlWords
+}
 
-const lines = ["Welcome home", "to V. where we build", "God’s house together."];
+const lines = ['Welcome home', 'to V. where we build', 'God’s house together.']
 
 export const Highlight = () => {
-  const splittedText = useMemo(
+  const splitText = useMemo(
     () =>
       lines
         .map((line, lineIdx) => {
-          const isLast = lineIdx === lines.length - 1;
-          const wordElements = splitText(
-            line + "\n",
-            isLast ? s["highlight"] : undefined
-          );
+          const isLast = lineIdx === lines.length - 1
+          const wordElements = splitTextFormat(
+            line + '\n',
+            isLast ? s['highlight'] : undefined
+          )
 
-          return wordElements;
+          return wordElements
         })
         .flat(),
     []
-  );
+  )
 
   return (
-    <Scrollytelling.Root
-      end="bottom bottom"
-      // debug={{
-      //   label: "Highlight" 
-      //   }}
-    >
-      <section className={s["spacer"]}>
-        <div className={s["pin"]}>
-          <p className={s["paragraph"]}>
+    <Scrollytelling.Root end="bottom bottom">
+      <section className={s['spacer']}>
+        <div className={s['pin']}>
+          <p className={s['paragraph']}>
             <Scrollytelling.Stagger
               overlap={0}
               tween={{
@@ -59,20 +54,20 @@ export const Highlight = () => {
                 end: 50,
                 fromTo: [
                   {
-                    opacity: 0.2,
+                    opacity: 0.2
                   },
                   {
                     opacity: 1,
-                    ease: "power2.out",
-                  },
-                ],
+                    ease: 'power2.out'
+                  }
+                ]
               }}
             >
-              {splittedText}
+              {splitText}
             </Scrollytelling.Stagger>
           </p>
         </div>
       </section>
     </Scrollytelling.Root>
-  );
-};
+  )
+}
