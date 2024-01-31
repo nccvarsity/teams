0fb7144b6c616a4ca627f4faba7f3c09420f6998
Timothy Chua <timothy.chua@22media.co>
2024-01-31 22:13:36 +0800

    Clean up lint

--- src/app/sections/marquee/index.tsx
+++ src/app/sections/marquee/index.tsx
@@ -1,24 +1,23 @@
-import * as Scrollytelling from "~/lib/scrollytelling-client";
-import s from "./marquee.module.scss";
-import { forwardRef } from "react";
+import { forwardRef } from 'react'
 
-const phrase = "     THE DREAM TEAM  →";
-const splitted = phrase.split("");
-const charsLength = splitted.length;
+import * as Scrollytelling from '~/lib/scrollytelling-client'
+
+import s from './marquee.module.scss'
+
+const phrase = '     THE DREAM TEAM  →'
+const splitPhrase = phrase.split('')
+const charsLength = splitPhrase.length
 
 export const Marquee = () => {
   return (
-    <Scrollytelling.Root
-      start="top top+=300px"
-      // debug={{ label: "Welcome" }}
-    >
+    <Scrollytelling.Root start="top top+=300px">
       <section className={s.section}>
         <div className={s.pinned}>
           <Scrollytelling.Animation
             tween={{
               start: 0,
               end: 90,
-              from: { xPercent: 98, ease: "linear" },
+              from: { xPercent: 98, ease: 'linear' }
             }}
           >
             <div className={s.animated}>
@@ -26,14 +25,14 @@ export const Marquee = () => {
                 tween={{
                   start: 90,
                   end: 100,
-                  to: { x: "-=50vw", ease: "linear" },
+                  to: { x: '-=50vw', ease: 'linear' }
                 }}
               >
                 <p>
-                  {splitted.map((s, i) => {
-                    const charDuration = 77 / charsLength;
-                    const charStart = charDuration * i;
-                    const charEnd = charStart + charDuration;
+                  {splitPhrase.map((s, i) => {
+                    const charDuration = 77 / charsLength
+                    const charStart = charDuration * i
+                    const charEnd = charStart + charDuration
 
                     return (
                       <Scrollytelling.Animation
@@ -46,30 +45,30 @@ export const Marquee = () => {
                               yPercent: 40,
                               scale: 0.5,
                               autoAlpha: 0,
-                              transformOrigin: "center right",
+                              transformOrigin: 'center right'
                             },
                             {
                               keyframes: {
-                                "0%": { autoAlpha: 0, scale: 0.5 },
-                                "50%": { autoAlpha: 1, scale: 1 },
-                                "100%": { yPercent: 0 },
-                                easeEach: "linear",
+                                '0%': { autoAlpha: 0, scale: 0.5 },
+                                '50%': { autoAlpha: 1, scale: 1 },
+                                '100%': { yPercent: 0 },
+                                easeEach: 'linear'
                               },
-                              ease: "linear",
-                            },
-                          ],
+                              ease: 'linear'
+                            }
+                          ]
                         }}
                       >
                         <span
                           data-character
                           style={{
-                            display: "inline-block",
+                            display: 'inline-block'
                           }}
                         >
-                          {s === " " ? <>&nbsp;</> : s}
+                          {s === ' ' ? <>&nbsp;</> : s}
                         </span>
                       </Scrollytelling.Animation>
-                    );
+                    )
                   })}
                 </p>
               </Scrollytelling.Animation>
@@ -79,24 +78,32 @@ export const Marquee = () => {
                   end: 100,
                   fromTo: [
                     { scale: 0.8, opacity: 0 },
-                    { scale: 1.45, opacity: 1, ease: "linear" },
-                  ],
+                    { scale: 1.45, opacity: 1, ease: 'linear' }
+                  ]
                 }}
               >
-                <TheSvg />
+                <VarsitySVG />
               </Scrollytelling.Animation>
             </div>
           </Scrollytelling.Animation>
         </div>
       </section>
     </Scrollytelling.Root>
-  );
-};
+  )
+}
 
-const TheSvg = forwardRef<SVGSVGElement>((_, ref) => {
+const VarsitySVG = forwardRef<SVGSVGElement>((_, ref) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg"viewBox="0 0 300 300" ref={ref} className={s.theSvg}>
-      <path fill="#FEFEFE" opacity="0.000000" stroke="none" 
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 300 300"
+      ref={ref}
+      className={s.theSvg}
+    >
+      <path
+        fill="#FEFEFE"
+        opacity="0.000000"
+        stroke="none"
         d="
       M175.000000,301.000000 
         C116.666679,301.000000 58.833355,301.000000 1.000023,301.000000 
@@ -110,8 +117,12 @@ const TheSvg = forwardRef<SVGSVGElement>((_, ref) => {
         C145.206787,275.146301 156.832153,275.146301 168.669174,275.146301 
         C192.273071,192.293106 215.803101,109.699173 239.477524,26.598368 
         C232.055237,26.598368 225.278076,26.598368 217.501190,26.620340 
-      z"/>
-      <path fill="#fff" opacity="1.000000" stroke="none" 
+      z"
+      />
+      <path
+        fill="#fff"
+        opacity="1.000000"
+        stroke="none"
         d="
       M218.001068,26.609354 
         C225.278076,26.598368 232.055237,26.598368 239.477524,26.598368 
@@ -125,8 +136,12 @@ const TheSvg = forwardRef<SVGSVGElement>((_, ref) => {
         C177.469681,187.177170 198.067368,114.860924 218.612305,42.729843 
         C173.208405,42.729843 128.498398,42.729843 83.256233,42.729843 
         C85.264854,49.785374 87.080231,56.162113 89.073128,63.329052 
-      z"/>
-      <path fill="#FDFDFD" opacity="0.000000" stroke="none" 
+      z"
+      />
+      <path
+        fill="#FDFDFD"
+        opacity="0.000000"
+        stroke="none"
         d="
       M88.984367,62.933952 
         C87.080231,56.162113 85.264854,49.785374 83.256233,42.729843 
@@ -134,7 +149,8 @@ const TheSvg = forwardRef<SVGSVGElement>((_, ref) => {
         C198.067368,114.860924 177.469681,187.177170 156.836823,259.616943 
         C152.938461,259.616943 149.303986,259.616943 145.098434,259.616943 
         C126.454422,194.296585 107.763779,128.812820 88.984367,62.933952 
-      z"/>
+      z"
+      />
     </svg>
-  );
-});
+  )
+})
